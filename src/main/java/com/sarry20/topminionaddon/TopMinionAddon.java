ca42edc062c3c2555312d7edcbd5c7fad13f1f86
sarry20 <izansr67@gmail.com>
2024-03-03 02:11:09 +0100

    feat: make uniqueminions increase limit

--- src/main/java/com/sarry20/topminionaddon/TopMinionAddon.java
+++ src/main/java/com/sarry20/topminionaddon/TopMinionAddon.java
@@ -1,17 +1,49 @@
 package com.sarry20.topminionaddon;
 
+import com.sarry20.lib.data.ConfigHelper;
+import com.sarry20.topminionaddon.listener.PlaceMinionListener;
+import com.sarry20.topminionaddon.manager.CPlayerManager;
+import com.sarry20.topminionaddon.model.CPlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TopMinionAddon extends JavaPlugin {
+    private ConfigHelper configUtil;
+    private static TopMinionAddon instance;
+    private CPlayerManager cPlayerManager;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
+        ConfigurationSerialization.registerClass(CPlayer.class);
+        getLogger().info("Plugin enabling");
+
+        configUtil = new ConfigHelper("plugins//TopMinionAddon//config.yml");
+        generateConfig();
+        cPlayerManager.loadCPlayers();
+        Bukkit.getPluginManager().registerEvents(new PlaceMinionListener(),this);
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        cPlayerManager.saveCPlayers();
+    }
+    private void generateConfig(){
+        if (configUtil.getYamlConfiguration().getInt("increaseLimit") == 0){
+            configUtil.getYamlConfiguration().set("increaseLimit",5);
+            configUtil.saveConfig();
+        }
+    }
+    public static TopMinionAddon getInstance() {
+        return instance;
+    }
+    public ConfigHelper getConfigUtil() {
+        return configUtil;
+    }
+
+    public CPlayerManager getcPlayerManager() {
+        return cPlayerManager;
     }
 }
---
+++ src/main/java/com/sarry20/topminionaddon/listener/PlaceMinionListener.java
@@ -0,0 +1,41 @@
+package com.sarry20.topminionaddon.listener;
+
+import com.sarry20.topminion.event.minion.MinionPlaceEvent;
+import com.sarry20.topminion.models.minion.MinionObj;
+import com.sarry20.topminion.models.minion.MinionObjManager;
+import com.sarry20.topminionaddon.TopMinionAddon;
+import com.sarry20.topminionaddon.model.CPlayer;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import java.util.List;
+
+public class PlaceMinionListener implements Listener {
+    private final TopMinionAddon plugin = TopMinionAddon.getInstance();
+    @EventHandler
+    public void atPlace(MinionPlaceEvent event){
+        CPlayer cPlayer = plugin.getcPlayerManager().getCPlayerByUUID(event.getPlayer().getUniqueId().toString());
+        if (event.getMinionsPlaced() >= event.getMinionsLimit()){
+            if (cPlayer == null)
+                return;
+            if (event.getMinionsPlaced() < event.getMinionsLimit()+cPlayer.getLimit()){
+                event.setSkipLimitCheck(true);
+            }
+        }
+        List<MinionObj> minions = MinionObjManager.getPlayerMinions(event.getPlayer().getName());
+        boolean isFirst = minions.stream().anyMatch(
+                minionObj ->
+                        minionObj.getType().equalsIgnoreCase(event.getType())
+        );
+        if (!isFirst)
+            return;
+        if (cPlayer == null)
+            return;
+        if (cPlayer.getPlacedTypes().contains(event.getType()))
+            return;
+        cPlayer.add1UniqueMinion(event.getType());
+        if (cPlayer.getUniqueMinions() >= plugin.getConfigUtil().getYamlConfiguration().getInt("increaseLimit")){
+            cPlayer.add1Limit();
+        }
+    }
+}
